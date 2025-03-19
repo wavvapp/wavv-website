5874f24cef122353bae4affb845844e8707e8fb4
igorntwari <igorntwari28@gmail.com>
2025-03-19 19:40:12 +0200

    refactor: remove empty spaces

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="backdrop-blur-md bg-black/30 border-t border-white/10 text-gray-300">
-      <div className="mx-auto px-14 flex h-10 items-center justify-between ">
+      <div className="mx-auto px-14 flex h-10 items-center justify-between">
         <p className="text-sm tracking-tight">&copy; 2024 Wavv</p>
         <p className="courser-pointer">
           <a href="mailto:hello@wavvapp.com">hello@wavvapp.com</a>
