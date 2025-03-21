8c35e6bc5c68a63613b166fcec2419344552c5d5
igorntwari <igorntwari28@gmail.com>
2025-03-21 09:43:28 +0200

    refactor: add proper responsiviness

--- app/components/FeatureCard.tsx
+++ app/components/FeatureCard.tsx
@@ -21,10 +21,10 @@ const FeatureCard = ({ feature, className }: FeatureCardProps) => (
     />
     <div className="absolute inset-0 bg-black/70" />
     <div className="relative xl:h-[80%] md:h-[90%] mt-auto p-6 flex flex-col justify-end z-10 max-w-[33rem] w-full">
-      <h3 className="text-3xl md:text-4xl lg:text-2xl xl:text-5xl font-anton font-normal text-white tracking-tight mb-3 overflow-hidden text-ellipsis">
+      <h3 className="text-3xl md:text-4xl lg:text-xl xl:text-5xl font-anton font-normal text-white tracking-tight mb-3 text-ellipsis">
         {feature.title}
       </h3>
-      <p className="text-normal md:text-xl lg:text-base xl:text-normal text-white font-mono leading-[1.2rem] overflow-hidden">
+      <p className="text-normal md:text-xl lg:text-lg xl:text-base text-white font-mono lg:leading-3 xl:leading-[1.2rem]">
         {feature.description}
       </p>
     </div>
